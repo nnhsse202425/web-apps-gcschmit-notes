ecc6b0da6458b8ac306445524d6231bb3ddaa94b
Geoff Schmit <gcschmit@gmail.com>
2024-11-20 11:27:40 -0600

    Create MongoClient
    load the environment variables for the project
    start to introduce async and await concepts

--- db.js
+++ db.js
@@ -3,3 +3,19 @@
 /**********************************************************
  * MongoDB
  **********************************************************/
+
+const { MongoClient } = require("mongodb");
+
+// load environment variables from .env file into process.env
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+async function run() {
+  try {
+    await client.connect();
+
+    
+  }
+}
